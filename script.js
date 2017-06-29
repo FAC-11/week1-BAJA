ae0f0d725ff48d9e7657f4957e8031548256c166
GlitterGlam <GlitterGlam@Glitters-MacBook-Pro.local>
2017-06-29 11:51:44 +0100

    refactored js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+// setting variable for navbar
+var navBarSelector = document.getElementById("myTopnav");
+
+// allows navbar to open and close on mobile devices
 function myFunction(button) {
     button.classList.toggle("change");
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+    navBarSelector.classList.toggle("responsive")
+
 }
