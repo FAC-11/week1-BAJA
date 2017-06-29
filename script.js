839480ec76b33fc5632b4d5d5baf785ee0ab31cb
GlitterGlam <GlitterGlam@Glitters-MacBook-Pro.local>
2017-06-29 12:46:02 +0100

    Navbar closes after page is selected

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-// setting variable for navbar
+// setting variable for navbar elements
 var navBarSelector = document.getElementById("myTopnav");
+var burgerSelector = document.getElementById("burger");
+var navLinksArraySelector = navBarSelector.querySelectorAll("A");
+
+window.onload = callEventListeners;
 
 // allows navbar to open and close on mobile devices
-function myFunction(button) {
-    button.classList.toggle("change");
+function toggleNavbar() {
+    burgerSelector.classList.toggle("change");
     navBarSelector.classList.toggle("responsive")
-
 }
+
+function closeNavbar(){
+  burgerSelector.classList.remove("change");
+  navBarSelector.classList.remove("responsive")
+}
+
+// Event list caller
+function callEventListeners(){
+  addBurgerListener();
+  addNavlinkListeners();
+}
+
+// Event listeners
+function addBurgerListener() {
+  burgerSelector.addEventListener("click",toggleNavbar);
+}
+
+function addNavlinkListeners(){
+
+  navLinksArraySelector.forEach(function(elem){
+    elem.addEventListener("click", closeNavbar);
+  })
+}
+
+
+
+// testing
+function callback(){console.log("YIPPEE")}
